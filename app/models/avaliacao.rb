9d8c5f7eb2e2ab6e16b48e8078e640c0ea80170b
Heitor Chans <heitorchans2@gmail.com>
2025-10-09 16:15:17 -0300

    nao vou mais pensar em nome

--- app/models/avaliacao.rb
+++ app/models/avaliacao.rb
@@ -1,4 +1,6 @@
 class Avaliacao < ApplicationRecord
+  has_many :curtida, dependent: :destroy
+
   belongs_to :usuario
   belongs_to :musica
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,4 @@
 class Usuario < ApplicationRecord
+  has_many :curtida, dependent: :destroy
+  has_many :avaliacao, dependent: :destroy
 end
